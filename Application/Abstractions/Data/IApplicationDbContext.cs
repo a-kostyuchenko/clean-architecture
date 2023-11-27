b93e69a30ae6fff35b38b022c07549f4b7d8c733
Alexey Kostyuchenko <kosttchka@gmail.com>
2023-11-27 19:37:36 +0100

    Added DbSets

--- Application/Abstractions/Data/IApplicationDbContext.cs
+++ Application/Abstractions/Data/IApplicationDbContext.cs
@@ -6,4 +6,6 @@ namespace Application.Abstractions.Data;
 public interface IApplicationDbContext
 {
     DbSet<User> Users { get; set; }
+    DbSet<Role> Roles { get; set; }
+    DbSet<Permission> Permissions { get; set; }
 }
--- Persistence/ApplicationDbContext.cs
+++ Persistence/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ public sealed class ApplicationDbContext : DbContext, IApplicationDbContext, IUn
     }
 
     public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
