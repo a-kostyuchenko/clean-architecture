fc814c1ce9da719d5c2940dee25bf661eacf6456
Alexey Kostyuchenko <kosttchka@gmail.com>
2023-11-28 17:46:46 +0100

    Added fluent validation extensions

---
+++ Application/Errors/ValidationErrors.cs
@@ -0,0 +1,7 @@
+namespace Application.Errors;
+
+internal static class ValidationErrors
+{
+    
+    
+}
---
+++ Application/Extensions/FluentValidationExtensions.cs
@@ -0,0 +1,15 @@
+using Domain.Shared;
+using FluentValidation;
+
+namespace Application.Extensions;
+
+public static class FluentValidationExtensions
+{
+    public static IRuleBuilderOptions<T, TProperty> WithError<T, TProperty>(
+        this IRuleBuilderOptions<T, TProperty> rule, Error error)
+    {
+        ArgumentNullException.ThrowIfNull(error, "The error is required");
+
+        return rule.WithErrorCode(error.Code).WithMessage(error.Message);
+    }
+}
