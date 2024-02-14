cf2889371f7da60c4547defe64909943ce39050b
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-02-14 11:57:57 +0100

    Removed file with cache keys

--- src/Persistence/Constants/CacheKeys.cs
+++
@@ -1,7 +0,0 @@
-namespace Persistence.Constants;
-
-public static class CacheKeys
-{
-    public static readonly string Users = "users";
-    public static readonly Func<Guid, string> UserById = userId => $"user-{userId}";
-}
